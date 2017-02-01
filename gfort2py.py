799e7e8e41163c6fec0b1b930fb70e019040a539
Robert Farmer <rjfarmer@asu.edu>
2017-02-01 14:45:16 -0700

    Fix getting explicit arrays

--- gfort2py.py
+++ gfort2py.py
@@ -1,8 +1,33 @@
 import ctypes
 import pickle
 import parseMod as pm
+import numpy as np
 		
 class fFort(object):
+	_GFC_MAX_DIMENSIONS=7
+	
+	_GFC_DTYPE_RANK_MASK=0x07
+	_GFC_DTYPE_TYPE_SHIFT=3
+	_GFC_DTYPE_TYPE_MASK=0x38
+	_GFC_DTYPE_SIZE_SHIFT=6
+	
+	_BT_UNKNOWN = 0
+	_BT_INTEGER=_BT_UNKNOWN+1 
+	_BT_LOGICAL=_BT_INTEGER+1
+	_BT_REAL=_BT_LOGICAL+1
+	_BT_COMPLEX=_BT_REAL+1
+	_BT_DERIVED=_BT_COMPLEX+1
+	_BT_CHARACTER=_BT_DERIVED+1
+	_BT_CLASS=_BT_CHARACTER+1
+	_BT_PROCEDURE=_BT_CLASS+1
+	_BT_HOLLERITH=_BT_PROCEDURE+1
+	_BT_VOID=_BT_HOLLERITH+1
+	_BT_ASSUMED=_BT_VOID+1	
+	
+	_index_t = ctypes.c_int64
+	_size_t = ctypes.c_int64	
+	
+	
 	def __init__(self,libname,fpy):
 		self.lib=ctypes.CDLL(libname)
 		self.libname=libname
@@ -37,7 +62,7 @@ class fFort(object):
 		
 	def _init_array(self,obj):
 		obj['array']['_ctype']=obj['_ctype']
-		obj['_ctype']=self._get_array_ctype(obj)
+		self._get_array_ctype(obj)
 	
 	def _init_dt(self,obj):
 		for i in obj['args']:
@@ -89,9 +114,13 @@ class fFort(object):
 	def _set_array(self,obj,value):
 		pass
 		
-	def _get_array(self,obj,array):
-		pass
-		
+	def _get_array(self,obj):
+		res=self._get_from_lib(obj)
+		if 'explicit' in obj['array']['atype']:
+			array=self._get_explicit_array(res,obj)
+		else:
+			pass
+		return array
 
 	def _call(self,name,*args):
 		#find function in self._funcs		
@@ -177,9 +206,9 @@ class fFort(object):
 	def _get_string_by_name(self,value,obj):
 		""" Gets a string"""
 		base_address=ctypes.addressof(value)
-		return self.__get_string_from_address(base_address)
+		return self._get_string_from_address(base_address)
 
-	def __get_string_from_address(self,ctype_address,debug=False):
+	def _get_string_from_address(self,ctype_address,debug=False):
 		out=''
 		i=0
 		while True:
@@ -192,17 +221,37 @@ class fFort(object):
 				out=out+(x.value).decode()
 				i=i+1
 		return out	
+		
+	def _get_explicit_array(self,res,obj):
+		shape=self._make_array_shape(obj)
+		array=[]
+		k=0
+		base_address=ctypes.addressof(res)
+		for i in range(len(shape)):
+			for j in range(shape[i]):
+				offset=base_address+k*ctypes.sizeof(obj['_ctype'])
+				array.append(obj['_ctype'].from_address(offset).value)
+				k=k+1
+		return np.reshape(array,newshape=shape)
 
+	def _make_array_shape(self,obj):
+		bounds=obj['array']['bounds']
+		shape=[]
+		for i,j in zip(bounds[0::2],bounds[1::2]):
+			shape.append(j-i+1)
+		return shape
+		
 
 	def _get_array_ctype(self,obj):
 		arr=obj['array']
-		obj['_ctype']=self.__make_array_ctype(arr['ndims'])
-		#Place to store the array after eing ctytpe-ifed
-		obj['_array']=obj['_ctype']()
-		#Initilize default values for this array
-		self.__init_array_ctype(obj)
+		if 'explicit' not in arr['atype']:
+			obj['_ctype']=self.__make_array_ctype(arr['ndims'])
+			#Place to store the array after ctytpe-ifed
+			obj['_array']=obj['_ctype']()
+			#Initilize default values for this array
+			self.__init_array_ctype(obj)
 		
-	def __make_array_ctype(self,ndim):
+	def _make_array_ctype(self,ndim):
 		class descriptor(ctypes.Structure):
 			_fields_=[("stride",self._index_t),
 					("lbound",self._index_t),
@@ -216,12 +265,12 @@ class fFort(object):
 					
 		return defarray	
 		
-	def __array_to_ctype(self,array,obj):
+	def _array_to_ctype(self,array,obj):
 		obj['_arrary'].base_addr=array.ctypes.data_as(ctypes.c_void_p)
 		
 		obj['_arrary'].offset=self.size_t(-1)
 		
-		obj['_arrary'].dtype=self.__get_dtype(obj)
+		obj['_arrary'].dtype=self._get_dtype(obj)
 				
 		for i in range(0,obj['array']['ndim']):
 			#Not sure if bounds are actually needed?
@@ -229,27 +278,26 @@ class fFort(object):
 			obj['_arrary'].dims[i].lbound=self._index_t(1)
 			obj['_arrary'].dims[i].ubound=self._index_t(array.shape[i])
 				
-	def __get_dtype(self,obj):
-		ftype=self.__get_ftype(obj)
+	def _get_dtype(self,obj):
+		ftype=self._get_ftype(obj)
 		dtype=obj['array']['ndims']
 		dtype=dtype|(ftype<<self.GFC_DTYPE_TYPE_SHIFT)
 		dtype=dtype|(ctypes.sizeof(obj['array']['_ctype'])<<self.GFC_DTYPE_SIZE_SHIFT)
 		return dtype
 
-	def __init_array_ctype(self,obj):		
+	def _init_array_ctype(self,obj):		
 		obj['_array'].base_addr=ctypes.c_void_p()
 		
-		obj['_array'].offset=self.size_t(-1)
+		obj['_array'].offset=self._size_t(-1)
 		obj['_array'].dtype=0
 				
-		for i in range(0,ndim):
-			obj['_array'].dims[i].stride=self.index_t(1)
-			obj['_array'].dims[i].lbound=self.index_t(1)
-			obj['_array'].dims[i].ubound=self.index_t(1)	
+		for i in range(0,obj['array']['ndims']):
+			obj['_array'].dims[i].stride=self._index_t(1)
+			obj['_array'].dims[i].lbound=self._index_t(1)
+			obj['_array'].dims[i].ubound=self._index_t(1)	
 
-		return _array,desc
 
-	def __get_ftype(self,obj):
+	def _get_ftype(self,obj):
 		dtype=obj['ctype']	
 		if 'c_int' in dtype:
 			ftype=self._BT_INTEGER
@@ -264,13 +312,13 @@ class fFort(object):
 		return ftype
 
 
-	def __ctype_to_array(self,value,obj):
+	def _ctype_to_array(self,value,obj):
 
 		sizebytes=ctypes.sizeof(obj['array']['_ctype'])
 		
 		shape=[]
 		stride=[]
-		for i in range(0,obj['array']['ndim']):
+		for i in range(0,obj['array']['ndims']):
 			shape.append((value.dims[i].ubound-
 						value.dims[i].lbound)+1)
 			stride.append(value.dims[0].stride*sizebytes)
@@ -289,12 +337,22 @@ class fFort(object):
 x=fFort('./test_mod.so','test_mod.fpy')
 
 x._init_var(x._mod_vars[11])
-if x._get_var(x._mod_vars[11]) is not 1:
-	print("Error")
-
+x._get_var(x._mod_vars[11])
 x._set_var(2,x._mod_vars[11])
+x._get_var(x._mod_vars[11]) 
 
-if x._get_var(x._mod_vars[11]) is not 2:
-	print("Error")
+
+x._mod_vars[0]
+
+#x._get_ctype(obj)
+
+#x._get_pytype(obj)
+#obj['array']['_ctype']=obj['_ctype']
+
+
+x._init_var(x._mod_vars[0])
+x._get_array(x._mod_vars[0]) 
+
+
 
 
