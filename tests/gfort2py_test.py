40f61e59910a6162ae50ec513fdb750a1cae1ce9
Robert Farmer <rjfarmer@asu.edu>
2017-03-12 15:45:25 -0700

    Add single pointers and targets in module vars

--- tests/gfort2py_test.py
+++ tests/gfort2py_test.py
@@ -151,6 +151,68 @@ class TestStringMethods(unittest.TestCase):
 		x.b_real_dp_exp_5d=v
 		np_test.assert_allclose(x.b_real_dp_exp_5d,v)
 
+	def test_a_int_point(self):
+		v=1
+		x.a_int_point=v
+		self.assertEqual(x.a_int_point,v)
+
+	def test_a_int_lp_point(self):
+		v=1
+		x.a_int_lp_point=v
+		self.assertEqual(x.a_int_lp_point,v)
+
+	def test_a_real_point(self):
+		v=1.0
+		x.a_real_point=v
+		self.assertEqual(x.a_real_point,v)
+		
+	def test_a_real_dp_point(self):
+		v=1.0
+		x.a_real_dp_point=v
+		self.assertEqual(x.a_real_dp_point,v)
+		
+	def test_a_real_qp_point(self):
+		v=1.0
+		x.a_real_qp_point=v
+		self.assertEqual(x.a_real_qp_point,v)
+		
+	def test_a_str_point(self):
+		v='abcdefghij'
+		x.a_str_point=v
+		self.assertEqual(x.a_str_point,v)
+
+	def test_a_int_target(self):
+		v=1
+		x.a_int_target=v
+		self.assertEqual(x.a_int_target,v)
+
+	def test_a_int_lp_target(self):
+		v=1
+		x.a_int_lp_target=v
+		self.assertEqual(x.a_int_lp_target,v)
+
+	def test_a_real_target(self):
+		v=1.0
+		x.a_real_target=v
+		self.assertEqual(x.a_real_target,v)
+		
+	def test_a_real_dp_target(self):
+		v=1.0
+		x.a_real_dp_target=v
+		self.assertEqual(x.a_real_dp_target,v)
+		
+	def test_a_real_qp_target(self):
+		v=1.0
+		x.a_real_qp_target=v
+		self.assertEqual(x.a_real_qp_target,v)
+		
+	def test_a_str_target(self):
+		v='abcdefghij'
+		x.a_str_target=v
+		self.assertEqual(x.a_str_target,v)
+
+
+
 if __name__ == '__main__':
 	unittest.main() 
 
