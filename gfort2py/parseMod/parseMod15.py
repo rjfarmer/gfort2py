873a869d8ac1c620ab78aeafe8fe61bed10fdf6b
Robert Farmer <r.j.farmer@uva.nl>
2018-07-22 20:43:39 +0200

    Fix param changes in mod_version 15

--- gfort2py/parseMod/parseMod15.py
+++ gfort2py/parseMod/parseMod15.py
@@ -1,11 +1,30 @@
 # SPDX-License-Identifier: GPL-2.0+
 
 from . import parseModCommon as pmc
-
+from .utils import *
+from .utils_cpython import *
 
 class parseMod(pmc.parseModBase):
-	def __init__(self,*args):
-		self.mod_version=14
-		super(parseMod,self).__init__(*args)
+    def __init__(self,*args):
+        self.mod_version=14
+        super(parseMod,self).__init__(*args)
 
+    def getParamValue(self,x,typ):
+        if 'ARRAY' in x:
+            x = split_brackets(x.strip())
+            arrLen = int(''.join(c for c in x[-2] if c not in "'()")) # Changed in mod15
+            x2 = split_brackets(x[1][x[1].index("("):])
+            listParam = [y.split("'")[-2] for y in x2]
+            p = [self.parseSingleParam(y,typ) for y in listParam]
+        else:
+            if 'COMPLEX' in x:
+                yy = x.split("'")
+                p = complex(self.parseSingleParam(yy[-4],'float'),
+                     self.parseSingleParam(yy[-2],'float'))
+            else:
+                y = x.split()[-2] # Changed in mod15
+                y = y.replace("'","")
+                p = self.parseSingleParam(y,typ) 
+            
+        return p
 
