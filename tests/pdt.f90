05a571b1dc1749088498979d909d5f8af2c9debf
Robert Farmer <r.j.farmer@uva.nl>
2020-04-14 20:33:02 +0200

    Add test cases for parametrized data types

---
+++ tests/pdt.f90
@@ -0,0 +1,38 @@
+! SPDX-License-Identifier: GPL-2.0+
+
+module pdt
+
+	use iso_fortran_env, only: output_unit, real128
+	
+	implicit none
+	
+	! Parameters
+	integer, parameter :: sp = selected_real_kind(p=8)
+	integer, parameter :: dp = selected_real_kind(p=15)
+	integer, parameter :: qp = selected_real_kind(p=30)
+	integer, parameter :: lp = selected_int_kind(8)
+	
+	
+	type pdt_def(k,a)
+		integer, kind :: k = sp 
+		integer, len :: a
+		real(k) :: array(a,a)
+	
+	end type pdt_def
+	
+	
+	type (pdt_def(dp,3)) :: pdt_dp_3
+	type (pdt_def(sp,3)) :: pdt_sp_3
+	
+	
+	contains
+
+
+	subroutine sub_write_pdt()
+	
+		write(*,*) pdt_dp_3%array
+	
+	end subroutine sub_write_pdt
+	
+
+end module pdt
