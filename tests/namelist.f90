fd2093097780e4ace95d9260fa1e15059c8df8e7
Robert Farmer <robert.j.farmer37@gmail.com>
2021-12-13 09:48:26 +0000

    Start on namelist testing

---
+++ tests/namelist.f90
@@ -0,0 +1,28 @@
+! SPDX-License-Identifier: GPL-2.0+
+
+module namelist
+
+	use iso_fortran_env, only: output_unit, real128
+	
+	implicit none
+	
+	! Parameters
+	integer, parameter :: dp = selected_real_kind(p=15)
+	integer, parameter :: qp = selected_real_kind(p=30)
+	integer, parameter :: lp = selected_int_kind(8)
+	
+
+	integer :: a_int
+	real :: b_real
+	real(dp) :: b_real_dp
+	character(len=100) :: c_str
+
+	namelist /namelist1/ a_int, b_real, b_real_dp, c_str
+
+	
+	contains
+
+
+
+end module namelist
+ 
