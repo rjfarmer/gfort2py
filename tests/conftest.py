a118ecc8860f1f156189df52d42bb739658159ee
Rob Farmer <robert.j.farmer37@gmail.com>
2022-01-05 11:38:37 +0000

    Add pytest option to skip skips

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,6 @@
 import subprocess
+import pytest
+import _pytest.skipping
 
 
 def pytest_configure(config):
@@ -8,3 +10,20 @@ def pytest_configure(config):
 
 def pytest_sessionfinish(session, exitstatus):
     subprocess.call(["make", "clean"], shell=True, cwd="tests")
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--no-skips",
+        action="store_true",
+        default=False, help="disable skip marks")
+
+
+@pytest.hookimpl(tryfirst=True)
+def pytest_cmdline_preparse(config, args):
+    if "--no-skips" not in args:
+        return
+
+    def no_skip(*args, **kwargs):
+        return
+
+    _pytest.skipping.skip = no_skip
