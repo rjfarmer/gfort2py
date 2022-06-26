0cc1bb5c137b17ce07b5c482d01838c94d8074e3
Robert Farmer <robert.j.farmer37@gmail.com>
2022-06-26 17:11:45 +0200

    Cleanup pytest conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,20 +10,3 @@ def pytest_configure(config):
 
 def pytest_sessionfinish(session, exitstatus):
     subprocess.call(["make", "clean"], shell=True, cwd="tests")
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--no-skips",
-        action="store_true",
-        default=False, help="disable skip marks")
-
-
-@pytest.hookimpl(tryfirst=True)
-def pytest_cmdline_preparse(config, args):
-    if "--no-skips" not in args:
-        return
-
-    def no_skip(*args, **kwargs):
-        return
-
-    _pytest.skipping.skip = no_skip
