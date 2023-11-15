cafac3b8f10b9efe1216800c308013a28c2bc2b9
Robert Farmer <robert.j.farmer37@gmail.com>
2023-11-15 16:05:43 +0000

    Fix gcc paths again

--- tests/compile_test.py
+++ tests/compile_test.py
@@ -1,6 +1,7 @@
 import os, sys
 import ctypes
 from pprint import pprint
+import platform
 
 os.environ["_GFORT2PY_TEST_FLAG"] = "1"
 
@@ -8,6 +9,11 @@ import numpy as np
 import gfort2py as gf
 
 import pytest
+
+if platform.system() == "Darwin":
+    FC = "/usr/local/bin/gfortran"
+else:
+    FC = "/usr/bin/gfortran"
 
 
 class TestCompileMethods:
@@ -19,7 +25,7 @@ class TestCompileMethods:
             end function myfunc
             """
 
-        x = gf.compile(string=fstr)
+        x = gf.compile(string=fstr, FC=FC)
 
         result = x.myfunc(1, 2)
 
@@ -36,21 +42,21 @@ class TestCompileMethods:
             end module
             """
 
-        x = gf.compile(string=fstr)
+        x = gf.compile(string=fstr, FC=FC)
 
         result = x.myfunc(1, 2)
 
         assert result.result == 3
 
     def test_compile_nomod_file(self):
-        x = gf.compile(file="tests/compile_nomod_test.f90")
+        x = gf.compile(file="tests/compile_nomod_test.f90", FC=FC)
 
         result = x.myfunc(1, 2)
 
         assert result.result == 3
 
     def test_compile_mod_file(self):
-        x = gf.compile(file="tests/compile_mod_test.f90")
+        x = gf.compile(file="tests/compile_mod_test.f90", FC=FC)
 
         result = x.myfunc(1, 2)
 
