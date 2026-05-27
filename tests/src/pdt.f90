07773ba96f951a47e97f5b3bced52aa059814a96
Robert Farmer <robert.j.farmer37@gmail.com>
2026-05-27 19:56:51 +0100

    tests: add PDT pass and return cases in pdt fixture

--- tests/src/pdt.f90
+++ tests/src/pdt.f90
@@ -27,6 +27,41 @@ module pdt
 	
 	contains
 
+	subroutine sub_pdt_dp_3_in(x)
+		type (pdt_def(dp,3)), intent(in) :: x
+
+		write(output_unit,*) x%array(1,1), x%array(3,3)
+	
+	end subroutine sub_pdt_dp_3_in
+
+	subroutine sub_pdt_dp_3_out(x)
+		type (pdt_def(dp,3)), intent(out) :: x
+
+		x%array = 0.0_dp
+		x%array(1,1) = 101.0_dp
+		x%array(2,2) = 202.0_dp
+		x%array(3,3) = 303.0_dp
+	
+	end subroutine sub_pdt_dp_3_out
+
+	subroutine sub_pdt_dp_3_inout(x)
+		type (pdt_def(dp,3)), intent(inout) :: x
+
+		x%array = x%array + 10.0_dp
+	
+	end subroutine sub_pdt_dp_3_inout
+
+	function func_return_pdt_dp_3() result(res)
+		type (pdt_def(dp,3)) :: res
+
+		res%array = reshape((/ &
+			11.0_dp, 21.0_dp, 31.0_dp, &
+			12.0_dp, 22.0_dp, 32.0_dp, &
+			13.0_dp, 23.0_dp, 33.0_dp  &
+		/), (/3,3/))
+	
+	end function func_return_pdt_dp_3
+
 
 	subroutine sub_write_pdt()
 	
